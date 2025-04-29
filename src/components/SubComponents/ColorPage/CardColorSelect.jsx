69dca5b1ffc4c1ec5ed0bf514f097391085c8543
wuerion <almerux@gmail.com>
2025-04-29 06:26:32 -0600

    creation component the card for was color

---
+++ src/components/SubComponents/ColorPage/CardColorSelect.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function CardColorSelect({ hexa }) {
+  return (
+    <div className="bg-linear-180 from-[#FFFFFF] to-[#1D1D1D] p-[1px] h-12">
+      <div className="bg-[#1D1D1D] flex items-center justify-evenly h-full text-white">
+        <p>Color:</p>
+        <p>{hexa}</p>
+        <div className={`rounded-lg w-8 h-8 bg-[${hexa}]`}></div>
+      </div>
+    </div>
+  );
+}
+
+export default CardColorSelect;
