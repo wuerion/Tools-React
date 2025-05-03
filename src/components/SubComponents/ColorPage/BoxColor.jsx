24ec992d703c1689bf93e098fd96eed814b21889
wuerion <almerux@gmail.com>
2025-05-02 19:54:02 -0600

    a function was create to make the rgb coexist and correct errors

--- src/components/SubComponents/ColorPage/BoxColor.jsx
+++ src/components/SubComponents/ColorPage/BoxColor.jsx
@@ -1,11 +1,21 @@
 import React, { createElement } from "react";
 
-function BoxColor({ color, onColorSelect }) {
+function BoxColor({ color, onColorSelect, onColorSelec }) {
   const handleClick = (selectedColor) => {
     onColorSelect(selectedColor);
+    const rgbColor = hexToRgb(selectedColor);
+    onColorSelec(rgbColor);
     navigator.clipboard.writeText(selectedColor); // Copia el color al portapapeles
     alert(`Color ${selectedColor} copied to clipboard`);
     console.log(selectedColor);
+  };
+
+  const hexToRgb = (hex) => {
+    const bigint = parseInt(hex.slice(1), 16);
+    const r = (bigint >> 16) & 255;
+    const g = (bigint >> 8) & 255;
+    const b = bigint & 255;
+    return `rgb(${r}, ${g}, ${b})`;
   };
 
   return (
