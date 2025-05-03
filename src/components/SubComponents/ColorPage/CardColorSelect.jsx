337c4b9cdef6b94483e7a9b6bf98b32b13fa0ec3
wuerion <almerux@gmail.com>
2025-05-02 19:52:24 -0600

    we create a second text for the rgb

--- src/components/SubComponents/ColorPage/CardColorSelect.jsx
+++ src/components/SubComponents/ColorPage/CardColorSelect.jsx
@@ -4,7 +4,7 @@ export function createColor(color) {
   return (
     <div>
       <div
-        className="w-5 h-5"
+        className="w-8 h-8 rounded-lg"
         style={{ backgroundColor: color }}
         title={`Background color: ${color}`}
       ></div>
@@ -12,12 +12,13 @@ export function createColor(color) {
   );
 }
 
-function CardColorSelect({ hexa }) {
+function CardColorSelect({ hexa, rgb }) {
   return (
     <div className="bg-linear-180 from-[#FFFFFF] to-[#1D1D1D] p-[1px] my-5 h-12">
       <div className="bg-[#1D1D1D] flex items-center justify-evenly h-full text-white">
         <p>Color:</p>
         <p>{hexa}</p>
+        <p>{rgb}</p>
         {createColor(hexa)} {/* Pasa el color como argumento */}
       </div>
     </div>
