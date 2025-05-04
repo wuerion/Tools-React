97bd9ddf9467b5f7c8772f3a27dde0c085c91ee2
wuerion <almerux@gmail.com>
2025-05-04 10:07:49 -0600

    update size height

--- src/components/SubComponents/ColorPage/Palettes/Palettes.jsx
+++ src/components/SubComponents/ColorPage/Palettes/Palettes.jsx
@@ -6,7 +6,7 @@ function palettes() {
   return (
     <section>
       <div className="bg-linear-180 from-[#FFFFFF] to-[#1D1D1D] w-full p-[1px] my-10">
-        <div className="bg-[#1d1d1d] w-full flex flex-col items-center justify-center text-white gap-10 py-10 px-4">
+        <div className="bg-[#1d1d1d] w-full h-96 flex flex-col items-center justify-center text-white gap-10 py-10 px-4">
           <h2 className="font-[Julius_Sans_One] text-4xl bg-clip-text text-transparent -bg-linear-270 from-[#B3BEFF] to-[#666666]">
             PALETTES
           </h2>
