40fbc0793a2de8fbc52ca41f5b072341a2007a05
wuerion <almerux@gmail.com>
2025-05-08 06:57:22 -0600

    agregar componente Test para selección y guardado de paletas de colores

--- src/components/SubComponents/ColorPage/Palettes/Palettes.jsx
+++ src/components/SubComponents/ColorPage/Palettes/Palettes.jsx
@@ -1,19 +1,56 @@
 import React from "react";
-import CardPaletteSelect from "./CardPaletteSelect";
 import GalleryPalettes from "./GalleryPalettes";
+import CreationPalettes from "./CreationPalettes";
 
 function palettes() {
   return (
     <section>
-      <div className="bg-linear-180 from-[#FFFFFF] to-[#1D1D1D] w-full p-[1px] my-10">
-        <div className="bg-[#1d1d1d] w-full h-96 flex flex-col items-center justify-center text-white gap-10 py-10 px-4">
-          <h2 className="font-[Julius_Sans_One] text-4xl bg-clip-text text-transparent -bg-linear-270 from-[#B3BEFF] to-[#666666]">
-            PALETTES
-          </h2>
-          <p>Here you can find variety of colors palettes</p>
-        </div>
-      </div>
+      <CreationPalettes />
       <GalleryPalettes />
+      {/* <div className="flex flex-col gap-5"> 
+      <div className="w-full h-12 border border-white flex justify-evenly items-center">
+        <div className="w-9 h-9 rounded-lg bg-white"></div>
+        <div className="w-9 h-9 rounded-lg bg-white"></div>
+        <div className="w-9 h-9 rounded-lg bg-white"></div>
+        <div className="w-9 h-9 rounded-lg bg-white"></div>
+      </div>
+      <div className="w-full h-12 border border-white flex justify-evenly items-center">
+        <div className="w-9 h-9 rounded-lg bg-white"></div>
+        <div className="w-9 h-9 rounded-lg bg-white"></div>
+        <div className="w-9 h-9 rounded-lg bg-white"></div>
+        <div className="w-9 h-9 rounded-lg bg-white"></div>
+      </div>
+      <div className="w-full h-12 border border-white flex justify-evenly items-center">
+        <div className="w-9 h-9 rounded-lg bg-white"></div>
+        <div className="w-9 h-9 rounded-lg bg-white"></div>
+        <div className="w-9 h-9 rounded-lg bg-white"></div>
+        <div className="w-9 h-9 rounded-lg bg-white"></div>
+      </div>
+      <div className="w-full h-12 border border-white flex justify-evenly items-center">
+        <div className="w-9 h-9 rounded-lg bg-white"></div>
+        <div className="w-9 h-9 rounded-lg bg-white"></div>
+        <div className="w-9 h-9 rounded-lg bg-white"></div>
+        <div className="w-9 h-9 rounded-lg bg-white"></div>
+      </div>
+      <div className="w-full h-12 border border-white flex justify-evenly items-center">
+        <div className="w-9 h-9 rounded-lg bg-white"></div>
+        <div className="w-9 h-9 rounded-lg bg-white"></div>
+        <div className="w-9 h-9 rounded-lg bg-white"></div>
+        <div className="w-9 h-9 rounded-lg bg-white"></div>
+      </div>
+      <div className="w-full h-12 border border-white flex justify-evenly items-center">
+        <div className="w-9 h-9 rounded-lg bg-white"></div>
+        <div className="w-9 h-9 rounded-lg bg-white"></div>
+        <div className="w-9 h-9 rounded-lg bg-white"></div>
+        <div className="w-9 h-9 rounded-lg bg-white"></div>
+      </div>
+      <div className="w-full h-12 border border-white flex justify-evenly items-center">
+        <div className="w-9 h-9 rounded-lg bg-white"></div>
+        <div className="w-9 h-9 rounded-lg bg-white"></div>
+        <div className="w-9 h-9 rounded-lg bg-white"></div>
+        <div className="w-9 h-9 rounded-lg bg-white"></div>
+      </div>
+       </div> */}
     </section>
   );
 }
