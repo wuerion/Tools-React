cc1ede65bae5d7c99026d78521594b1b4f40fc07
wuerion <almerux@gmail.com>
2025-05-08 21:37:52 -0600

    eliminar el componente CardGradientSelect y mantener solo GalleryGradients en la página de gradientes

--- src/pages/GradientPage.jsx
+++ src/pages/GradientPage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { usePageTitleAndText } from "../context/PageTitleAndTextContext";
 import GalleryGradients from "../components/SubComponents/GradientPage/GalleryGradients";
-import CardGradientSelect from "../components/SubComponents/GradientPage/CardGradientSelect";
+
 function GradientPage() {
   const { setPageTitle } = usePageTitleAndText();
   const { setPageText } = usePageTitleAndText();
@@ -19,12 +19,7 @@ function GradientPage() {
   }, [setMenuIs]);
   return (
     <div>
-      <CardGradientSelect
-        color1={"#a4f9c7"}
-        color2={"#acf6f6"}
-        color3={"#a2bbef"}
-        color4={"#bea3ef"}
-      />
+      <GalleryGradients />
     </div>
   );
 }
