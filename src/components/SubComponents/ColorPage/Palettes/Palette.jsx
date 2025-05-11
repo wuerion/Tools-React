2b37dc0bc727a5715d2e4b27994cb3c53b0b57fc
wuerion <almerux@gmail.com>
2025-05-11 11:09:59 -0600

    added new val

--- src/components/SubComponents/ColorPage/Palettes/Palette.jsx
+++ src/components/SubComponents/ColorPage/Palettes/Palette.jsx
@@ -16,17 +16,20 @@ function Palette({
   color2 = "#" + color2.toUpperCase();
   color3 = "#" + color3.toUpperCase();
   color4 = "#" + color4.toUpperCase();
-  color5 = "#" + color5.toUpperCase();
+  color5 = color5 ? color5.toUpperCase() : "";
+
+  const x = color5 !== undefined && color5 !== null && color5 !== "";
   // function for copy palet
   //? podemos ingresar los colores de la paleta en un arrar dentro de otro array, le asignamos al array un tipo de ID con el map lo buscamos y hacemos considir el ID con el num del array (el primer array va atener el ID 0)
 
-  const handleClick = (color1, color2, color3, color4, color5) => {
-    onPaletteSelect1(color1);
-    onPaletteSelect2(color2);
-    onPaletteSelect3(color3);
-    onPaletteSelect4(color4);
-    onPaletteSelect5(color5);
+  const handleClick = (color1, color2, color3, color4, color5 = "") => {
+    if (typeof onPaletteSelect1 === "function") onPaletteSelect1(color1);
+    if (typeof onPaletteSelect2 === "function") onPaletteSelect2(color2);
+    if (typeof onPaletteSelect3 === "function") onPaletteSelect3(color3);
+    if (typeof onPaletteSelect4 === "function") onPaletteSelect4(color4);
+    if (typeof onPaletteSelect5 === "function") onPaletteSelect5(color5);
   };
+  const color = [color5 ? [color5] : ""];
 
   return (
     <>
@@ -41,7 +44,7 @@ function Palette({
           {
             className:
               "bg-[#1D1D1D] flex items-center justify-evenly h-full py-1.5 text-white",
-            onClick: () => handleClick(color1, color2, color3, color4, color5),
+            onClick: () => handleClick(color1, color2, color3, color4, color),
           },
           createElement("div", {
             className: "w-9 h-9 rounded-lg",
