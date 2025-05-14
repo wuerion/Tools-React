e4d7b11b9c63e9d83f2d2fc4891a14f4bfa59141
wuerion <almerux@gmail.com>
2025-05-13 20:36:40 -0600

    added style for dark mode

--- src/components/SubComponents/ColorPage/Piker/ColorLine.jsx
+++ src/components/SubComponents/ColorPage/Piker/ColorLine.jsx
@@ -5,7 +5,9 @@ import React, { createElement } from "react";
 function ColorLine({ color, nameColor }) {
   return (
     <div className="w-full">
-      <p className="text-center my-5 text-lg text-white">{nameColor}</p>
+      <p className="text-center my-5 text-lg text-[#1D1D1D] dark:text-[#DDDAD8]">
+        {nameColor}
+      </p>
       <div className="flex items-end w-full flex-col gap-2.5">
         {color.map((col, i) =>
           createElement(
@@ -13,7 +15,7 @@ function ColorLine({ color, nameColor }) {
             {
               key: i,
               className:
-                "text-white w-full flex border border-white border-r-0 py-1 pl-1",
+                "text-[#1D1D1D] dark:text-[#DDDAD8] w-full flex border border-[#1D1D1D] dark:border-[#DDDAD8] border-r-0 py-1 pl-1",
             },
             createElement("div", {
               className: "w-5 h-14 min-w-[20px]",
@@ -23,7 +25,7 @@ function ColorLine({ color, nameColor }) {
               "div",
               {
                 className:
-                  "bg-[#1D1D1D] flex flex-col items-start justify-end px-2 min-w-5 w-[100px]",
+                  "bg-[#DDDA8] flex flex-col items-start justify-end px-2 min-w-5 w-[100px]",
               },
               createElement("p", {}, "HEXA"),
               createElement("p", {}, col)
