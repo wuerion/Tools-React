2a829f114c1bf1a68c6671e9ed308e29d7cd9955
wuerion <almerux@gmail.com>
2025-05-14 21:05:49 -0600

    added style for dark mode

--- src/components/SubComponents/WebsPageAndPlugins/CreateBox.jsx
+++ src/components/SubComponents/WebsPageAndPlugins/CreateBox.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function CreateBox({ title, text, url, img, alt }) {
   return (
-    <div className="bg-linear-180 from-[#FFFFFF] to-[#1D1D1D] p-[1px]  my-5 w-full">
-      <div className="bg-[#1D1D1D] flex flex-col items-center justify-center gap-1 p-2 h-full w-full text-white cursor-pointer">
+    <div className="bg-linear-180 from-[#1D1D1D] to-[#DDDAD8] dark:from-[#FFFFFF] dark:to-[#1D1D1D] p-[1px]  my-5 w-full">
+      <div className="bg-[#DDDAD8] dark:bg-[#1D1D1D] flex flex-col items-center justify-center gap-1 p-2 h-full w-full text-[#181818] dark:text-[#DDDAD8] cursor-pointer">
         <h3>{title}</h3>
         <a href={`${url}`} className="w-3/4 my-4" target="_blank">
           <img src={`${img}`} alt={`${alt}`} />
