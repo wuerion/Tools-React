120f28c2db5e7ad44a9842001d4350315b7ce61c
wuerion <almerux@gmail.com>
2025-05-14 19:57:46 -0600

    eliminar el contenedor de la interfaz de usuario en GalleryPalettes

--- src/components/SubComponents/ColorPage/Palettes/GalleryPalettes.jsx
+++ src/components/SubComponents/ColorPage/Palettes/GalleryPalettes.jsx
@@ -3,15 +3,6 @@ import CardPaletteSelect from "./CardPaletteSelect";
 import Palette from "./Palette";
 
 function GalleryPalettes() {
-  <div className="bg-linear-180 from-[#FFFFFF] to-[#1D1D1D] w-full p-[1px] my-10">
-    <div className="bg-[#1d1d1d] w-full h-96 flex flex-col items-center justify-center text-white gap-10 py-10 px-4">
-      <h2 className="font-[Julius_Sans_One] text-4xl bg-clip-text text-transparent -bg-linear-270 from-[#B3BEFF] to-[#666666]">
-        PALETTES
-      </h2>
-      <p>Here you can find variety of colors palettes</p>
-    </div>
-  </div>;
-
   // creamos un children
   const [selectedPaletteColor1, setSelectedPaletteColor1] = useState("");
   const [selectedPaletteColor2, setSelectedPaletteColor2] = useState("");
