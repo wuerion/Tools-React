4839bf0836bc5a9b7b5bf2931243522072808124
wuerion <almerux@gmail.com>
2025-05-14 21:02:30 -0600

    added style for dark mode

--- src/components/SubComponents/FrameAndLibra/BoxFrameLibra.jsx
+++ src/components/SubComponents/FrameAndLibra/BoxFrameLibra.jsx
@@ -2,8 +2,8 @@ import React, { children } from "react";
 
 function BoxFrameLibra({ url, children }) {
   return (
-    <div className="bg-linear-180 from-[#FFFFFF] to-[#1D1D1D] p-[1px] my-5 h-16 w-full">
-      <div className="bg-[#1D1D1D] flex flex-col items-center justify-center gap-1 h-20 w-full text-white cursor-pointer">
+    <div className="bg-linear-180 from-[#1D1D1D] to-[#DDDAD8] dark:from-[#FFFFFF] dark:to-[#1D1D1D] p-[1px] my-5 h-16 w-full">
+      <div className="bg-[#DDDAD8] dark:bg-[#1D1D1D] flex flex-col items-center justify-center gap-1 h-20 w-full text-white cursor-pointer">
         <a href={`${url}`}>{children}</a>
       </div>
     </div>
