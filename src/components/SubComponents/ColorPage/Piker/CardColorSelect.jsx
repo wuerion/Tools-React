5f164148e90b6f5020163a8c8d4ab8e2b69d1294
wuerion <almerux@gmail.com>
2025-05-17 10:48:09 -0600

    added support for hsl color code

--- src/components/SubComponents/ColorPage/Piker/CardColorSelect.jsx
+++ src/components/SubComponents/ColorPage/Piker/CardColorSelect.jsx
@@ -12,13 +12,14 @@ export function createColor(color) {
   );
 }
 
-function CardColorSelect({ hexa, rgb }) {
+function CardColorSelect({ hexa, rgb, hsl }) {
   return (
-    <div className="bg-linear-180 from-[#1D1D1D] to-[#DDDA8] dark:from-[#FFFFFF] dark:to-[#1D1D1D] p-[1px] my-5 h-12">
+    <div className="bg-linear-180 from-[#1D1D1D] to-[#DDDA8] dark:from-[#FFFFFF] dark:to-[#1D1D1D] p-[1px] my-5 h-12 w-full">
       <div className="bg-[#DDDAD8] dark:bg-[#1D1D1D] flex items-center justify-evenly h-full text-[#181818] dark:text-[#DDDAD8]">
         <p>Color:</p>
         <p>{hexa}</p>
         <p>{rgb}</p>
+        <p className="hidden md:block">{hsl}</p>
         {createColor(hexa)} {/* Pasa el color como argumento */}
       </div>
     </div>
