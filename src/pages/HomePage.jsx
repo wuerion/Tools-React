c53fe266facc0c0d06037d0a65d3b090d1174a2c
wuerion <almerux@gmail.com>
2025-05-18 07:42:09 -0600

    added description text

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,7 +4,9 @@ import { usePageTitleAndText } from "../context/PageTitleAndTextContext.jsx"; //
 function HomePage() {
   // obtiene la funcion para actualizar el titulo del contexto
   const { setPageTitle } = usePageTitleAndText();
+  const { setTextTitle } = usePageTitleAndText();
   const { setPageText } = usePageTitleAndText();
+  const { setDescription } = usePageTitleAndText();
   const { setMenuIs } = usePageTitleAndText();
 
   // useEffect se ejecura cunado el componente se monta el componente
@@ -17,9 +19,18 @@ function HomePage() {
 
   // devuelve SOLO el contenido espeecifico de la paguina
   // no el layout completo
+
+  useEffect(() => {
+    setTextTitle(
+      "Do you need color, gradients, fonts, frameworks, web, plugins?"
+    );
+  }, [setTextTitle]);
   useEffect(() => {
     setPageText("It`s your site, here you can willl find what do you need.");
   }, [setPageText]);
+  useEffect(() => {
+    setDescription(true);
+  }, [setDescription]);
   useEffect(() => {
     setMenuIs(true);
   }, [setMenuIs]);
