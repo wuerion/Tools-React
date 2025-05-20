0ad5513520f64fdd27bf0a9eb2027f013871d44b
wuerion <almerux@gmail.com>
2025-05-19 21:16:42 -0600

    remove code comment

--- src/components/SubComponents/ColorPage/Palettes/Palettes.jsx
+++ src/components/SubComponents/ColorPage/Palettes/Palettes.jsx
@@ -7,50 +7,6 @@ function palettes() {
     <section className="w-full">
       <CreationPalettes />
       <GalleryPalettes />
-      {/* <div className="flex flex-col gap-5"> 
-      <div className="w-full h-12 border border-white flex justify-evenly items-center">
-        <div className="w-9 h-9 rounded-lg bg-white"></div>
-        <div className="w-9 h-9 rounded-lg bg-white"></div>
-        <div className="w-9 h-9 rounded-lg bg-white"></div>
-        <div className="w-9 h-9 rounded-lg bg-white"></div>
-      </div>
-      <div className="w-full h-12 border border-white flex justify-evenly items-center">
-        <div className="w-9 h-9 rounded-lg bg-white"></div>
-        <div className="w-9 h-9 rounded-lg bg-white"></div>
-        <div className="w-9 h-9 rounded-lg bg-white"></div>
-        <div className="w-9 h-9 rounded-lg bg-white"></div>
-      </div>
-      <div className="w-full h-12 border border-white flex justify-evenly items-center">
-        <div className="w-9 h-9 rounded-lg bg-white"></div>
-        <div className="w-9 h-9 rounded-lg bg-white"></div>
-        <div className="w-9 h-9 rounded-lg bg-white"></div>
-        <div className="w-9 h-9 rounded-lg bg-white"></div>
-      </div>
-      <div className="w-full h-12 border border-white flex justify-evenly items-center">
-        <div className="w-9 h-9 rounded-lg bg-white"></div>
-        <div className="w-9 h-9 rounded-lg bg-white"></div>
-        <div className="w-9 h-9 rounded-lg bg-white"></div>
-        <div className="w-9 h-9 rounded-lg bg-white"></div>
-      </div>
-      <div className="w-full h-12 border border-white flex justify-evenly items-center">
-        <div className="w-9 h-9 rounded-lg bg-white"></div>
-        <div className="w-9 h-9 rounded-lg bg-white"></div>
-        <div className="w-9 h-9 rounded-lg bg-white"></div>
-        <div className="w-9 h-9 rounded-lg bg-white"></div>
-      </div>
-      <div className="w-full h-12 border border-white flex justify-evenly items-center">
-        <div className="w-9 h-9 rounded-lg bg-white"></div>
-        <div className="w-9 h-9 rounded-lg bg-white"></div>
-        <div className="w-9 h-9 rounded-lg bg-white"></div>
-        <div className="w-9 h-9 rounded-lg bg-white"></div>
-      </div>
-      <div className="w-full h-12 border border-white flex justify-evenly items-center">
-        <div className="w-9 h-9 rounded-lg bg-white"></div>
-        <div className="w-9 h-9 rounded-lg bg-white"></div>
-        <div className="w-9 h-9 rounded-lg bg-white"></div>
-        <div className="w-9 h-9 rounded-lg bg-white"></div>
-      </div>
-       </div> */}
     </section>
   );
 }
