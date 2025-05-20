653815a23c1492209ad7876b2bc8179392aa238e
wuerion <almerux@gmail.com>
2025-05-19 21:08:17 -0600

    rename component  notification

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Notify({ text }) {
+function Notification({ text }) {
   return (
     <div className="fixed bottom-20 flex items-center justify-center w-full -m-5">
       <p className="bg-[#1D1D1D] text-[#DDDAD8] dark:bg-[#DDDAD8] dark:text-[#181818] w-1/2 h-10 absolute rounded-lg flex items-center justify-center text-center">
@@ -10,4 +10,4 @@ function Notify({ text }) {
   );
 }
 
-export default Notify;
+export default Notification;
