2b8fbfdf711fb40ecd9eeaf2f2bbc3113c64d59c
wuerion <almerux@gmail.com>
2025-05-22 19:33:56 -0600

    added responsive desing

--- src/components/SubComponents/WebsPageAndPlugins/CreateBox.jsx
+++ src/components/SubComponents/WebsPageAndPlugins/CreateBox.jsx
@@ -2,15 +2,19 @@ import React from "react";
 
 function CreateBox({ title = "", text = "", url, img, alt }) {
   return (
-    <div className="bg-linear-180 from-[#1D1D1D] to-[#DDDAD8] dark:from-[#FFFFFF] dark:to-[#1D1D1D] p-[1px]  my-5 w-full h-full md:w-4/5">
-      <div className="bg-[#DDDAD8] dark:bg-[#1D1D1D] flex flex-col items-center justify-center gap-1 p-2 h-full w-full text-[#181818] dark:text-[#DDDAD8] cursor-pointer">
+    <div className="bg-linear-180 from-[#1D1D1D] to-[#DDDAD8] dark:from-[#FFFFFF] dark:to-[#1D1D1D] p-[1px] my-5 w-full h-full md:w-4/5 lg:w-3/4">
+      <div className="bg-[#DDDAD8] dark:bg-[#1D1D1D] flex flex-col items-center justify-center gap-5 p-2 h-full w-full text-[#181818] dark:text-[#DDDAD8] cursor-pointer">
         <h3>{title}</h3>
         <a
           href={`${url}`}
-          className="w-3/4 h-[100px] md:w-1/2 my-4 flex justify-center items-center"
+          className="w-3/4 h-[100px] my-4 flex justify-center items-center"
           target="_blank"
         >
-          <img src={`${img}`} alt={`${alt}`} className="w-full h-full" />
+          <img
+            src={`${img}`}
+            alt={`${alt}`}
+            className="w-full h-full md:h-4/5"
+          />
         </a>
         <p>{text}</p>
       </div>
