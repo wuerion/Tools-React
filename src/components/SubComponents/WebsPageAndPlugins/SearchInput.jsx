a1b3a948f35575425b78b9f082c7a6728b90c01d
wuerion <almerux@gmail.com>
2025-05-22 19:32:53 -0600

    added responsive desing\

--- src/components/SubComponents/WebsPageAndPlugins/SearchInput.jsx
+++ src/components/SubComponents/WebsPageAndPlugins/SearchInput.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 
-function ShearchInput({ seacrhItem, changeSearchItem }) {
+function SearchInput({ seacrhItem, changeSearchItem }) {
   return (
-    <div className="bg-[#DDDAD8] dark:bg-[#1D1D1D] flex flex-col items-center justify-center gap-4 h-full text-[#181818] dark:text-[#DDDAD8] p-2">
-      <div></div>
-      <div className="bg-[#f2f2f2] dark:bg-white/30 w-full flex justify-between rounded-tl-4xl rounded-lg  shadow-[0_4px_2px_#DAD7D5] dark:shadow-[0_4px_2px_#181818]">
-        <div className="w-full">
+    <div className="bg-[#DDDAD8] dark:bg-[#1D1D1D] flex flex-col items-center justify-center gap-4 h-full text-[#181818] dark:text-[#dddad8] p-2">
+      <div className="bg-[#f2f2f2] dark:bg-white/30 w-full md:w-4/5 lg:w-3/4 h-10 md:h-11 flex justify-between rounded-tl-4xl rounded-lg shadow-[0_4px_2px_#DAD7D5] dark:shadow-[0_4px_2px_#181818]">
+        <div className="w-full ">
           <input
             type="search"
             name="search"
@@ -15,12 +14,12 @@ function ShearchInput({ seacrhItem, changeSearchItem }) {
             value={seacrhItem}
             onChange={changeSearchItem}
             aria-label="Type something..."
-            className=" w-full h-10 px-5 focus:outline-0 rounded-tl-4xl rounded-br-4xl  text-[#DDDAD8] dark:text-[#181818] bg-[#181818] dark:bg-[#DDDAD8]"
+            className=" w-full h-10 md:h-11 px-5 focus:outline-0 rounded-tl-4xl rounded-br-4xl  text-[#DDDAD8] dark:text-[#181818] bg-[#181818] dark:bg-[#DDDAD8]"
           />
         </div>
         <div className="flex items-center justify-center w-16">
           <a
-            href={``}
+            href={changeSearchItem}
             target="_blank"
             title="Google Fonts"
             aria-label="Google Fonts"
@@ -35,4 +34,4 @@ function ShearchInput({ seacrhItem, changeSearchItem }) {
   );
 }
 
-export default ShearchInput;
+export default SearchInput;
